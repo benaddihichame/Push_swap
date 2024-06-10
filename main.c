ad4afcb950cab80ad5a8ac72c58bd5ceba722add
hichame <hichame@macbook.local>
2024-06-10 18:06:11 +0200

    first

---
+++ main.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbenaddi <hbenaddi@student.42lehavre.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/19 02:06:37 by hbenaddi          #+#    #+#             */
+/*   Updated: 2024/06/10 15:41:26 by hbenaddi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_stack    *insert_int(t_stack sort)
+{
+    t_stack *new;
+
+    new = malloc(sizeof(t_stack));
+}
+int main(int ac, char **av)
+{
+    t_stack a;
+}
---
+++ push_swap.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hbenaddi <hbenaddi@student.42lehavre.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/18 18:54:52 by hbenaddi          #+#    #+#             */
+/*   Updated: 2024/06/10 15:33:37 by hbenaddi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct s_stack
+{
+    int     num;
+    struct s_stack  *next;
+}             t_stack;
+
+#endif
