3abcdf2824532ade2209cd41a967d00ad33ede7c
hichame <hichame@macbook.local>
2024-08-19 13:12:32 +0200

    kkkkkkk

--- src/1rotate.c
+++ src/1rotate.c
@@ -6,7 +6,7 @@
 /*   By: hbenaddi <hbenaddi@student.42lehavre.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 15:55:36 by hbenaddi          #+#    #+#             */
-/*   Updated: 2024/08/13 15:38:46 by hbenaddi         ###   ########.fr       */
+/*   Updated: 2024/08/18 11:33:29 by hbenaddi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ void	rb(t_stack **b)
 	t_stack	*tmp;
 	t_stack	*current;
 
-	tmp = *b;
-	*b = tmp->next;
-	tmp->next = NULL;
-	current = *b;
-	while (current->next)
+	tmp = *b;  // Stocke le premier élément de la pile b dans tmp
+	*b = tmp->next;  // Le deuxième élément devient le nouveau premier
+	tmp->next = NULL;  // Déconnecte tmp du reste de la pile
+	current = *b;  // Initialise current au nouveau premier élément
+	while (current->next)  // Parcourt la pile jusqu'au dernier élément
 		current = current->next;
-	current->next = tmp;
-	ft_printf("rb\n");
+	current->next = tmp;  // Connecte l'ancien premier élément à la fin
+	ft_printf("rb\n");  // Affiche l'opération effectuée
 }
 
 void	ra(t_stack **a)
@@ -32,12 +32,12 @@ void	ra(t_stack **a)
 	t_stack	*tmp;
 	t_stack	*current;
 
-	tmp = *a;
-	*a = tmp->next;
-	tmp->next = NULL;
-	current = *a;
-	while (current->next)
+	tmp = *a;  // Stocke le premier élément de la pile a dans tmp
+	*a = tmp->next;  // Le deuxième élément devient le nouveau premier
+	tmp->next = NULL;  // Déconnecte tmp du reste de la pile
+	current = *a;  // Initialise current au nouveau premier élément
+	while (current->next)  // Parcourt la pile jusqu'au dernier élément
 		current = current->next;
-	current->next = tmp;
-	ft_printf("ra\n");
+	current->next = tmp;  // Connecte l'ancien premier élément à la fin
+	ft_printf("ra\n");  // Affiche l'opération effectuée
 }
